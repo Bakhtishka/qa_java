371b8e209dc2a6ad4797961a3fa8feb0bbf00998
Bakhtiyor Abdullayev <bakhtishka@yandex.ru>
2023-02-16 20:23:17 +0300

    "first commit"

--- src/main/java/com/example/LionAlex.java
+++ src/main/java/com/example/LionAlex.java
@@ -1,2 +1,26 @@
-package com.example;public class LionAlex {
+package com.example;
+
+
+import java.util.List;
+
+
+public class LionAlex extends Lion {
+
+    public LionAlex(Feline feline, String sex) throws Exception {
+        super(feline, sex);
+    }
+
+    @Override
+    public int getKittens() {
+        return 0;
+    }
+
+
+    public List<String> getFriends() {
+        return List.of("Глория", "Мэлман", "Марти");
+    }
+
+    public String getPlaceOfLiving() {
+        return "Нью Йорк";
+    }
 }
--- src/test/java/com/example/LionAlexTest.java
+++ src/test/java/com/example/LionAlexTest.java
@@ -1,4 +1,36 @@
+package com.example;
+
+import org.junit.Test;
+
+import java.util.List;
+
 import static org.junit.Assert.*;
+
 public class LionAlexTest {
-  
+    Feline feline = new Feline();
+    LionAlex alex = new LionAlex(feline, "Самец");
+
+    public LionAlexTest() throws Exception {
+    }
+
+    @Test
+    public void testGetKittensShouldReturnZero() {
+
+        assertEquals(0, alex.getKittens());
+    }
+
+    @Test
+    public void getFriendsShouldReturnFriends() {
+        List<String> actual = alex.getFriends();
+        List<String> expected = List.of("Глория", "Мэлман", "Марти");
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void getPlaceOfLivingShouldReturnLivingPlace() {
+        String expected = "Нью Йорк";
+        String actual = alex.getPlaceOfLiving();
+        assertEquals(expected, actual);
+    }
 }
